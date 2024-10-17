f2f53f52213f7ef8997607ca582ab51cfd811181
Ben Hillis <benhill@ntdev.microsoft.com>
2024-10-17 12:52:41 -0700

    Test using a new repo-scoped 1ES pool

--- flowey/flowey_hvlite/src/pipelines_shared/gh_pools.rs
+++ flowey/flowey_hvlite/src/pipelines_shared/gh_pools.rs
@@ -25,7 +25,7 @@ pub fn default_gh_hosted(platform: FlowPlatform) -> GhRunner {
 pub fn windows_amd_self_hosted() -> GhRunner {
     GhRunner::SelfHosted(vec![
         "self-hosted".to_string(),
-        "1ES.Pool=HvLite-GitHub-Win-Pool-WestUS3".to_string(),
+        "1ES.Pool=OpenVMM-GitHub-Win-Pool-WestUS3".to_string(),
     ])
 }
 
@@ -43,7 +43,7 @@ pub fn windows_intel_self_hosted() -> GhRunner {
 pub fn windows_amd_self_hosted_largedisk() -> GhRunner {
     GhRunner::SelfHosted(vec![
         "self-hosted".to_string(),
-        "1ES.Pool=HvLite-GitHub-Win-Pool-WestUS3".to_string(),
+        "1ES.Pool=OpenVMM-GitHub-Win-Pool-WestUS3".to_string(),
         "1ES.ImageOverride=HvLite-CI-Win-Ge-Image-256GB".to_string(),
     ])
 }
