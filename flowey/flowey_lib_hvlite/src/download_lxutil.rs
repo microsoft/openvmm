2d669a1c08550c4ca16a82739e5bc084175caa08
Daniel Prilik <daprilik@microsoft.com>
2024-10-21 14:33:51 -0700

    xtask fmt --fix

--- flowey/flowey_lib_hvlite/src/download_lxutil.rs
+++ flowey/flowey_lib_hvlite/src/download_lxutil.rs
@@ -64,7 +64,8 @@ impl FlowNode for Node {
             return Ok(());
         }
 
-        let extract_archive_deps = flowey_lib_common::_util::extract::extract_archive_if_new_deps(ctx);
+        let extract_archive_deps =
+            flowey_lib_common::_util::extract::extract_archive_if_new_deps(ctx);
 
         for (arch, out_vars) in reqs {
             let tag = format!("Microsoft.WSL.LxUtil.{}", version);
