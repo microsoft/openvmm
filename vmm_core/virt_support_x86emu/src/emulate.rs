5e49648bc5a56c200f0e37992a718f91b0a13747
John Starks <jostarks@microsoft.com>
2024-10-29 12:19:17 -0700

    Update vmm_core/virt_support_x86emu/src/emulate.rs

--- vmm_core/virt_support_x86emu/src/emulate.rs
+++ vmm_core/virt_support_x86emu/src/emulate.rs
@@ -397,7 +397,7 @@ pub async fn emulate<T: EmulatorSupport>(
                 ));
             }
             x86emu::Error::InstructionException(exception, error_code, cause) => {
-                tracelimit::debug!(
+                tracing::trace!(
                     ?exception,
                     ?error_code,
                     ?cause,
