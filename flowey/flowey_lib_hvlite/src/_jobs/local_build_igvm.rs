139ace7cf30759de7bb8b897952fc022b14f2d91
John Starks <jostarks@microsoft.com>
2024-11-01 17:28:15 +0000

    flowey: build-igvm: allow passing additional rootfs.config files
    This is useful for including additional files in the root file system
    for testing.

--- flowey/flowey_lib_hvlite/src/_jobs/local_build_igvm.rs
+++ flowey/flowey_lib_hvlite/src/_jobs/local_build_igvm.rs
@@ -32,6 +32,7 @@ pub struct Customizations {
     pub custom_sidecar: Option<PathBuf>,
     pub custom_uefi: Option<PathBuf>,
     pub custom_vtl0_kernel: Option<PathBuf>,
+    pub additional_rootfs: Vec<PathBuf>,
     pub override_arch: Option<CommonArch>,
     pub override_kernel_pkg: Option<OpenhclKernelPackage>,
     pub override_manifest: Option<PathBuf>,
@@ -93,6 +94,7 @@ impl SimpleFlowNode for Node {
             with_debuginfo,
             with_perf_tools,
             with_sidecar,
+            additional_rootfs,
         } = customizations;
 
         let profile = if release {
@@ -147,6 +149,10 @@ impl SimpleFlowNode for Node {
                 custom_uefi: custom_uefi.map(|p| p.absolute()).transpose()?,
                 custom_kernel: custom_kernel.map(|p| p.absolute()).transpose()?,
                 custom_sidecar: custom_sidecar.map(|p| p.absolute()).transpose()?,
+                additional_rootfs: additional_rootfs
+                    .into_iter()
+                    .map(|p| p.absolute())
+                    .collect::<Result<_, _>>()?,
             });
 
             if let Some(p) = override_manifest {
