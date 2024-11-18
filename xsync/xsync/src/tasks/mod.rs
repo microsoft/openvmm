c7d1d75b2e961e628fac29767e5ffede9bcc18cd
Steven Malis <stevenmalis@microsoft.com>
2024-11-18 13:21:43 -0500

    Add support forx syncing rust-toolchain.toml files

--- xsync/xsync/src/tasks/mod.rs
+++ xsync/xsync/src/tasks/mod.rs
@@ -5,6 +5,8 @@
 
 pub mod cargo_lock;
 pub mod cargo_toml;
+pub mod rust_toolchain_toml;
 
 pub use self::cargo_lock::CargoLock;
 pub use self::cargo_toml::CargoToml;
+pub use self::rust_toolchain_toml::RustToolchainToml;
