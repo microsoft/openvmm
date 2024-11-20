f3f680181fd3959b86ddcfbd61e314f68af279a8
Kirpal Grewal <kirpal@outlook.com>
2024-11-20 13:50:25 +0000

    indexing

--- vm/devices/user_driver/src/emulated.rs
+++ vm/devices/user_driver/src/emulated.rs
@@ -260,7 +260,7 @@ pub struct EmulatedDmaAllocator {
 impl HostDmaAllocator for EmulatedDmaAllocator {
     fn allocate_dma_buffer(&self, len: usize) -> anyhow::Result<MemoryBlock> {
         let memory = MemoryBlock::new(self.shared_mem.alloc(len).context("out of memory")?);
-        memory.as_slice()[0..].atomic_fill(0);
+        memory.as_slice().atomic_fill(0);
         Ok(memory)
     }
 }
