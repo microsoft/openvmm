baaf917f05fe3d14b1826c812b1e4ecb69568432
John Starks <jostarks@microsoft.com>
2024-12-17 03:53:58 +0000

    fix

--- vm/devices/net/gdma/src/queues.rs
+++ vm/devices/net/gdma/src/queues.rs
@@ -96,14 +96,15 @@ impl<T: AsBytes> CqEq<T> {
         let offset = (self.tail & (self.cap - 1)) as usize * size_of::<T>();
         let mut range = self.region.range();
         range.skip(offset);
-        let entry = entry.as_bytes();
-        if let Err(err) = range.writer(gm).write(&entry[..entry.len() - 1]) {
+        let mut writer = range.writer(gm);
+        let (entry, last) = entry.as_bytes().split_at(entry.as_bytes().len() - 1);
+        if let Err(err) = writer.write(entry) {
             tracing::warn!(err = &err as &dyn std::error::Error, "failed to write");
         }
         // Write the final byte last after a release fence to ensure that the
         // guest sees the entire entry before the owner count is updated.
         std::sync::atomic::fence(Release);
-        if let Err(err) = range.writer(gm).write(&[entry[entry.len() - 1]]) {
+        if let Err(err) = writer.write(last) {
             tracing::warn!(err = &err as &dyn std::error::Error, "failed to write");
         }
         // Ensure the write is flushed before sending the interrupt.
