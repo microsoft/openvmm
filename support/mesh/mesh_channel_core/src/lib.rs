2dc4cb5b53fe66a46f4d416bdb8e3ea806b05ff5
John Starks <jostarks@microsoft.com>
2025-01-02 17:56:05 +0000

    comment

--- support/mesh/mesh_channel_core/src/lib.rs
+++ support/mesh/mesh_channel_core/src/lib.rs
@@ -1,7 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-//! Core mesh channel functionality.
+//! Core mesh channel functionality, supporting sending and receiving messages
+//! within and between nodes.
+//!
+//! This contains only the basic channel implementations, not the extra utility
+//! types on top that `mesh_channel` provides.
 
 #![warn(missing_docs)]
 
