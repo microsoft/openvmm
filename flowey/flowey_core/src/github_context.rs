d88b355d0213c8c49a832c7cd1814a29e3e640af
Justus Camp <campjustus@microsoft.com>
2025-01-15 11:25:32 -0800

    shared helper

--- flowey/flowey_core/src/github_context.rs
+++ flowey/flowey_core/src/github_context.rs
@@ -4,7 +4,7 @@
 //! Core types and traits used to read GitHub context variables.
 
 use crate::node::{user_facing::GhContextVar, ClaimVar, NodeCtx, ReadVar, StepCtx};
-use serde::{Deserialize, Serialize};
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use std::collections::BTreeMap;
 
 #[derive(Serialize, Deserialize)]
@@ -34,45 +34,14 @@ pub struct GhContextVarReader<'a, S> {
     pub _state: std::marker::PhantomData<S>,
 }
 
-impl<'a> GhContextVarReader<'a, Root> {
-    pub fn global(&self, gh_var: GhContextVar) -> ReadVar<String> {
-        let (var, write_var) = self.ctx.new_maybe_secret_var(gh_var.is_secret(), "");
-        let write_var = write_var.claim(&mut StepCtx {
-            backend: self.ctx.backend.clone(),
-        });
-        let var_state = GhVarState {
-            raw_name: gh_var.as_raw_var_name(),
-            backing_var: write_var.backing_var,
-            is_secret: write_var.is_secret,
-            is_object: false,
-        };
-        let gh_to_rust = vec![var_state];
-
-        self.ctx.backend.borrow_mut().on_emit_gh_step(
-            &format!("🌼 read {}", gh_var.as_raw_var_name()),
-            "",
-            BTreeMap::new(),
-            None,
-            BTreeMap::new(),
-            BTreeMap::new(),
-            gh_to_rust,
-            Vec::new(),
-        );
-        var
-    }
-
-    pub fn event(self) -> GhContextVarReader<'a, Event> {
-        GhContextVarReader {
-            ctx: self.ctx,
-            _state: std::marker::PhantomData,
-        }
-    }
-}
-
-impl GhContextVarReader<'_, Event> {
-    pub fn pull_request(self) -> ReadVar<Option<GhContextVarReaderEventPullRequest>> {
-        let var_name = "github.event.pull_request".to_string();
-        let (var, write_var) = self.ctx.new_var();
+impl<'a, S> GhContextVarReader<'a, S> {
+    fn read_var<T: Serialize + DeserializeOwned>(
+        &self,
+        var_name: String,
+        is_secret: bool,
+        is_object: bool,
+    ) -> ReadVar<T> {
+        let (var, write_var) = self.ctx.new_maybe_secret_var(is_secret, "");
         let write_var = write_var.claim(&mut StepCtx {
             backend: self.ctx.backend.clone(),
         });
@@ -80,9 +49,8 @@ impl GhContextVarReader<'_, Event> {
             raw_name: var_name.clone(),
             backing_var: write_var.backing_var,
             is_secret: write_var.is_secret,
-            is_object: true,
+            is_object,
         };
-
         let gh_to_rust = vec![var_state];
 
         self.ctx.backend.borrow_mut().on_emit_gh_step(
@@ -96,5 +64,24 @@ impl GhContextVarReader<'_, Event> {
             Vec::new(),
         );
         var
+    }
+}
+
+impl<'a> GhContextVarReader<'a, Root> {
+    pub fn global(&self, gh_var: GhContextVar) -> ReadVar<String> {
+        self.read_var(gh_var.as_raw_var_name(), gh_var.is_secret(), false)
+    }
+
+    pub fn event(self) -> GhContextVarReader<'a, Event> {
+        GhContextVarReader {
+            ctx: self.ctx,
+            _state: std::marker::PhantomData,
+        }
+    }
+}
+
+impl GhContextVarReader<'_, Event> {
+    pub fn pull_request(self) -> ReadVar<Option<GhContextVarReaderEventPullRequest>> {
+        self.read_var("github.event.pull_request".to_string(), false, true)
     }
 }
