9be44a95a3a0a4ae480b9499785f01d1dbd641df
Justus Camp <campjustus@gmail.com>
2025-01-29 23:07:37 +0000

    try another xshell cmd to get around macro not working properly

--- flowey/flowey_lib_common/src/gh_workflow_id.rs
+++ flowey/flowey_lib_common/src/gh_workflow_id.rs
@@ -50,49 +50,60 @@ impl SimpleFlowNode for Node {
 
             move |rt| {
                 let github_commit_hash = rt.read(github_commit_hash);
-            let sh = xshell::Shell::new()?;
-            let gh_token = rt.read(gh_token);
-            let repo_path = rt.read(repo_path);
-            let gh_cli = rt.read(gh_cli);
+                let sh = xshell::Shell::new()?;
+                let gh_token = rt.read(gh_token);
+                let repo_path = rt.read(repo_path);
+                let gh_cli = rt.read(gh_cli);
 
-            sh.change_dir(repo_path);
-            // Fetches the CI build workflow id for a given commit hash
+                sh.change_dir(repo_path);
 
-            let get_action_id = |commit: String| {
-                let cmd = format!(
-                    "{} run list --commit {} -w '{}' -s 'completed' -L 1 --json databaseId --jq '.[].databaseId'",
-                    gh_cli.display(),
-                    commit,
-                    pipeline_name
-                );
-                sh.cmd(cmd).env("GITHUB_TOKEN", gh_token.clone()).read()
-            };
+                // Fetches the CI build workflow id for a given commit hash
+                let get_action_id = |commit: String| {
+                    sh.cmd(gh_cli.clone())
+                        .arg("run")
+                        .arg("list")
+                        .arg("--commit")
+                        .arg(commit)
+                        .arg("-w")
+                        .arg(&pipeline_name)
+                        .arg("-s")
+                        .arg("completed")
+                        .arg("-L")
+                        .arg("1")
+                        .arg("--json")
+                        .arg("databaseId")
+                        .arg("--jq")
+                        .arg(".[].databaseId")
+                        .env("GITHUB_TOKEN", gh_token.clone())
+                        .read()
+                };
 
-            let mut github_commit_hash = github_commit_hash.clone();
-            let mut action_id = get_action_id(github_commit_hash.clone());
-            let mut loop_count = 0;
+                let mut github_commit_hash = github_commit_hash.clone();
+                let mut action_id = get_action_id(github_commit_hash.clone());
+                let mut loop_count = 0;
 
-            // CI may not have finished the build for the merge base, so loop through commits
-            // until we find a finished build or fail after 5 attempts
-            while let Err(ref e) = action_id {
-                if loop_count > 4 {
-                    anyhow::bail!("Failed to get action id after 5 attempts: {}", e);
+                // CI may not have finished the build for the merge base, so loop through commits
+                // until we find a finished build or fail after 5 attempts
+                while let Err(ref e) = action_id {
+                    if loop_count > 4 {
+                        anyhow::bail!("Failed to get action id after 5 attempts: {}", e);
+                    }
+
+                    github_commit_hash =
+                        xshell::cmd!(sh, "git rev-parse {github_commit_hash}^").read()?;
+                    action_id = get_action_id(github_commit_hash.clone());
+                    loop_count += 1;
                 }
 
-                github_commit_hash = xshell::cmd!(sh, "git rev-parse {github_commit_hash}^").read()?;
-                action_id = get_action_id(github_commit_hash.clone());
-                loop_count += 1;
-            }
+                if let Ok(id) = action_id {
+                    print!("Got action id: {}", id);
+                    rt.write(gh_workflow_id, &id);
+                } else {
+                    anyhow::bail!("Failed to get action id");
+                }
 
-            if let Ok(id) = action_id {
-                print!("Got action id: {}", id);
-                rt.write(gh_workflow_id, &id);
-            } else {
-                anyhow::bail!("Failed to get action id");
+                Ok(())
             }
-
-            Ok(())
-        }
         });
 
         Ok(())
