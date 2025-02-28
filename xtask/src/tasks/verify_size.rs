89cf96001ed2f51c76c2a164dadce54a71045a9a
Steven Malis <stevenmalis@microsoft.com>
2025-02-28 15:48:32 -0500

    feedback

--- xtask/src/tasks/verify_size.rs
+++ xtask/src/tasks/verify_size.rs
@@ -93,8 +93,15 @@ impl Xtask for VerifySize {
 
         println!("Total difference: {total_diff} KiB.");
 
-        if total_diff > 50 {
-            anyhow::bail!("{} size verification failed: The total difference ({} KiB) is greater than the allowed difference ({} KiB).", self.new.display(), total_diff, 50);
+        const ALLOWED: u64 = 50;
+        if total_diff > ALLOWED {
+            anyhow::bail!(
+                "{} size verification failed: \
+            The total difference ({} KiB) is greater than the allowed difference ({} KiB).",
+                self.new.display(),
+                total_diff,
+                ALLOWED
+            );
         }
 
         Ok(())
