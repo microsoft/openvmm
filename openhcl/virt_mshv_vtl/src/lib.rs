ffd17d8fb1ebcbf9c5d8de5863754379178dda67
Steven Malis <stevenmalis@microsoft.com>
2025-03-12 12:58:04 -0400

    Fallthrough for cpuid

--- openhcl/virt_mshv_vtl/src/lib.rs
+++ openhcl/virt_mshv_vtl/src/lib.rs
@@ -1850,7 +1850,11 @@ impl UhProtoPartition<'_> {
         match params.isolation {
             IsolationType::None | IsolationType::Vbs => {}
             #[cfg(guest_arch = "x86_64")]
-            IsolationType::Tdx => return Ok(params.env_cvm_guest_vsm),
+            IsolationType::Tdx => {
+                if !params.env_cvm_guest_vsm {
+                    return Ok(false);
+                }
+            }
             #[cfg(guest_arch = "x86_64")]
             IsolationType::Snp => {
                 if !params.env_cvm_guest_vsm {
