4c264fd3a2fd00a17fd1f80bbeec45ededf3c8ed
Guramrit Singh <gurasingh@microsoft.com>
2025-03-17 16:40:44 -0700

    Separating out the emulated logic from user driver

---
+++ vm/devices/dma_guest_memory_wrapper/src/lib.rs
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+//! Provides wrapper functionality over guest memory for testing purposes only
+
+#[cfg(test)]
+mod emulated;
