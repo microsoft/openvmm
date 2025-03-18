f009d06e56dea4c5c2a9c34ebd247f45e64f09c7
John Starks <jostarks@microsoft.com>
2025-03-18 17:35:52 +0000

    tmk: add simple VMM for running TMKs
    To make it easier to test core `virt_*` functionality without all the
    accoutrements of a full-featured VMM (virtual devices, etc.), create a
    VMM that can just run simple test microkernels (TMKs) and not full guest
    operating systems.
    
    This VMM can run either on the host or in a paravisor environment.
    
    Current limitations:
    
    * No docs on using this
    * No integration with CI test pass
    * x86_64 only
    * Non-CVM only
    * Just define one simple TMK that doesn't test much of anything
    
    Ideally, this will mesh up with the effort to define some rich TMKs for
    use with full-features VMMs, so that we can run some subset of TMKs in
    both environments. But even without that, this will be valuable for
    platform bringup.

---
+++ tmk/tmk_vmm/src/main.rs
@@ -0,0 +1,73 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+//! A simple VMM for loading and running test microkernels (TMKs) but does not
+//! support general-purpose VMs.
+//!
+//! This is used to test the underlying VMM infrastructure without the complexity
+//! of the full OpenVMM stack.
+
+// UNSAFETY: needed to map guest memory.
+#![expect(unsafe_code)]
+
+mod host_vmm;
+mod load;
+mod paravisor_vmm;
+mod run;
+
+use clap::Parser;
+use pal_async::DefaultDriver;
+use pal_async::DefaultPool;
+use run::CommonState;
+use std::path::PathBuf;
+
+fn main() -> anyhow::Result<()> {
+    DefaultPool::run_with(do_main)
+}
+
+#[derive(Parser)]
+struct Options {
+    /// The hypervisor to use.
+    #[clap(long)]
+    hv: HypervisorOpt,
+    /// The path to the TMK binary.
+    #[clap(long)]
+    tmk: PathBuf,
+}
+
+#[derive(clap::ValueEnum, Clone)]
+enum HypervisorOpt {
+    #[cfg(target_os = "linux")]
+    Kvm,
+    #[cfg(all(target_os = "linux", guest_arch = "x86_64"))]
+    Mshv,
+    #[cfg(target_os = "linux")]
+    MshvVtl,
+    #[cfg(target_os = "windows")]
+    Whp,
+    #[cfg(target_os = "macos")]
+    Hvf,
+}
+
+async fn do_main(driver: DefaultDriver) -> anyhow::Result<()> {
+    let opts = Options::parse();
+
+    tracing_subscriber::fmt()
+        .fmt_fields(tracing_helpers::formatter::FieldFormatter)
+        .init();
+
+    let mut state = CommonState::new(driver, opts).await?;
+
+    match state.opts.hv {
+        #[cfg(target_os = "linux")]
+        HypervisorOpt::Kvm => state.run_host_vmm(virt_kvm::Kvm).await,
+        #[cfg(all(target_os = "linux", guest_arch = "x86_64"))]
+        HypervisorOpt::Mshv => state.run_host_vmm(virt_mshv::LinuxMshv).await,
+        #[cfg(target_os = "linux")]
+        HypervisorOpt::MshvVtl => state.run_paravisor_vmm(virt::IsolationType::None).await,
+        #[cfg(windows)]
+        HypervisorOpt::Whp => state.run_host_vmm(virt_whp::Whp).await,
+        #[cfg(target_os = "macos")]
+        HypervisorOpt::Hvf => state.run_host_vmm(virt_hvf::HvfHypervisor).await,
+    }
+}
