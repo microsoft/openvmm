af8e9133f9e9d4b11a2038987996b7add9a9a777
Steven Malis <stevenmalis@microsoft.com>
2025-04-03 12:18:39 -0400

    Fix

--- vm/devices/firmware/hcl_compat_uefi_nvram_storage/src/lib.rs
+++ vm/devices/firmware/hcl_compat_uefi_nvram_storage/src/lib.rs
@@ -526,6 +526,7 @@ mod test {
         .unwrap();
         let name_too_big = Ucs2LeVec::from_vec_with_nul(
             std::iter::repeat_n([0, b'a'], EFI_MAX_VARIABLE_NAME_SIZE / 2)
+                .chain(Some([0, 0]))
                 .flat_map(|x| x.into_iter())
                 .collect(),
         )
