fa02089ce24a9c498deb8976843e2eb9ec9076a2
Ming-Wei Shih <mishih@microsoft.com>
2025-04-15 22:04:43 +0000

    update comment
    Signed-off-by: Ming-Wei Shih <mishih@microsoft.com>

--- vm/devices/tpm/src/logger.rs
+++ vm/devices/tpm/src/logger.rs
@@ -36,9 +36,12 @@ impl<T: 'static + TpmLogger> From<T> for ResolvedTpmLogger {
 #[async_trait::async_trait]
 pub trait TpmLogger: Send + Sync {
     /// Send an event with the given id to the host and flush.
+    /// This is a non-blocking call that can be used in an async context.
     async fn log_event_and_flush(&self, event: TpmLogEvent);
 
     /// Send an event with the given id to the host without flushing.
+    /// This is a blocking call that can be used in a non-async context
+    /// such as the callback of `PollDevice::poll_device` for AK cert request.
     fn log_event(&self, event: TpmLogEvent);
 }
 
