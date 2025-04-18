d4adf3d10c3cf317dae01faa5868688f881df5ea
John Starks <jostarks@microsoft.com>
2025-04-18 21:39:37 +0000

    feedback,fmt

--- vmm_tests/vmm_tests/tests/tmks.rs
+++ vmm_tests/vmm_tests/tests/tmks.rs
@@ -4,6 +4,10 @@
 //! Test entrypoint for running TMK tests in different environments.
 
 // Include all the tests.
+//
+// FUTURE: probably the tmk_tests package should own this crate, rather than
+// reuse vmm_tests. But this creates a bunch of work for CI. Revisit this once
+// CI is a little less cumbersome to modify.
 use tmk_tests as _;
 
 fn main() {
