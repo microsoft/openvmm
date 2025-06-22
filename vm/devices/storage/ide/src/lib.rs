e0ac3ba2af9f986e12067daefa4e8b4fd24dcbd7
Eric Newberry <ericnewberry@microsoft.com>
2025-06-22 17:28:33 +0000

    Remove set_prd_exhausted

--- vm/devices/storage/ide/src/lib.rs
+++ vm/devices/storage/ide/src/lib.rs
@@ -772,7 +772,6 @@ impl Channel {
                 dma.descriptor_idx += 1;
                 if dma.transfer_complete {
                     dma.descriptor_idx = 0;
-                    drive.set_prd_exhausted();
                 }
             }
 
