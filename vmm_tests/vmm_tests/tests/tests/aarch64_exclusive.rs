ef8b18e844903ef7078e0a5516dda0a8c54faa1a
Chris Oo <cho@microsoft.com>
2025-07-29 22:15:17 +0000

    fix build

--- vmm_tests/vmm_tests/tests/tests/aarch64_exclusive.rs
+++ vmm_tests/vmm_tests/tests/tests/aarch64_exclusive.rs
@@ -4,7 +4,7 @@
 //! Integration tests for aarch64 guests.
 
 use petri::PetriVmBuilder;
-use petri::openvmm::OpenVmmPetriBackend;
+use petri::PetriVmmBackend;
 use petri::pipette::cmd;
 use vmm_core_defs::HaltReason;
 use vmm_test_macros::vmm_test;
@@ -20,7 +20,7 @@ use vmm_test_macros::vmm_test;
     openvmm_uefi_aarch64(vhd(ubuntu_2404_server_aarch64)),
     hyperv_uefi_aarch64(vhd(ubuntu_2404_server_aarch64)),
 )]
-async fn pmu_gsiv(config: PetriVmBuilder<OpenVmmPetriBackend>) -> Result<(), anyhow::Error> {
+async fn pmu_gsiv<T: PetriVmmBackend>(config: PetriVmBuilder<T>) -> Result<(), anyhow::Error> {
     let (vm, agent) = config.run().await?;
 
     // Check dmesg for logs about the PMU.
