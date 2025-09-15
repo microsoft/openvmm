e979aa37058c3803ba025cf84e66e7b6e6c9f5d4
Hadi Rahal-Arabi <hadi.rahal-arabi@intel.com>
2025-09-15 16:33:50 -0700

    no_std page table builder

--- vm/loader/page_table/src/lib.rs
+++ vm/loader/page_table/src/lib.rs
@@ -3,11 +3,24 @@
 
 //! Methods to construct page tables.
 
+#![no_std]
 #![expect(missing_docs)]
 #![forbid(unsafe_code)]
 
 pub mod aarch64;
 pub mod x64;
+
+use thiserror::Error;
+
+/// Errors returned by the Page Table Builder
+#[derive(Debug, PartialEq, Eq, Error)]
+pub enum Error {
+    /// The PageTableBuilder bytes buffer does not match the size of the struct buffer
+    #[error(
+        "PageTableBuilder bytes buffer size [{bytes_buf}] does not match the struct buffer size [{struct_buf}]"
+    )]
+    BadBufferSize { bytes_buf: usize, struct_buf: usize },
+}
 
 /// Size of the initial identity map
 #[derive(Debug, Copy, Clone)]
