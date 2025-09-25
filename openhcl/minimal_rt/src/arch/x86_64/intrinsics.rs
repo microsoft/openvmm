fd0528a32f17cec43d9a5fe877b06271208e6e14
Chris Oo <cho@microsoft.com>
2025-09-25 11:30:00 -0700

    minimal_rt: fix memcpy to correctly return original dest

--- openhcl/minimal_rt/src/arch/x86_64/intrinsics.rs
+++ openhcl/minimal_rt/src/arch/x86_64/intrinsics.rs
@@ -29,7 +29,7 @@ unsafe extern "C" fn memset(mut ptr: *mut u8, val: i32, len: usize) -> *mut u8 {
 // SAFETY: The minimal_rt_build crate ensures that when this code is compiled
 // there is no libc for this to conflict with.
 #[unsafe(no_mangle)]
-unsafe extern "C" fn memcpy(mut dest: *mut u8, src: *const u8, len: usize) -> *mut u8 {
+unsafe extern "C" fn memcpy(dest: *mut u8, src: *const u8, len: usize) -> *mut u8 {
     // SAFETY: The caller guarantees that the pointers and length are correct.
     unsafe {
         core::arch::asm!(r#"
@@ -38,7 +38,7 @@ unsafe extern "C" fn memcpy(mut dest: *mut u8, src: *const u8, len: usize) -> *m
             "#,
             in("rsi") src,
             in("rcx") len,
-            inout("rdi") dest);
+            inout("rdi") dest => _);
     }
     dest
 }
