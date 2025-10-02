d2de0da452c91b126245743e806f30d096435d84
mfrohlich <mfrohlich@microsoft.com>
2025-10-02 14:15:02 -0700

    add license headers

--- vm/devices/tdisp/src/devicereport.rs
+++ vm/devices/tdisp/src/devicereport.rs
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 use bitfield_struct::bitfield;
 use zerocopy::{FromBytes, Immutable, KnownLayout};
 
--- vm/devices/tdisp/src/serialize.rs
+++ vm/devices/tdisp/src/serialize.rs
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 use zerocopy::{FromBytes, Immutable, IntoBytes, KnownLayout, TryFromBytes};
 
 use crate::command::{
