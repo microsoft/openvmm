6b45cfe6d2c3d9aca4e15931f910428cf48d4152
Matt LaFayette (Kurjanowicz) <mattkur@microsoft.com>
2025-10-07 02:28:35 +0000

    petri: make `inspect` public, and vmm_tests: use `inspect` to verify some state of the nvme driver in basic relay scenarios.
    TODO:
    
    - [ ] Move the `child` helper to the `inspect` crate?
    - [x] Fix up test failures (test failures are expected at this point, since the
          logic isn't solid.
    - [ ] Use this technique in more interesting ways.

--- vmm_tests/vmm_tests/tests/tests/x86_64/openhcl_uefi.rs
+++ vmm_tests/vmm_tests/tests/tests/x86_64/openhcl_uefi.rs
@@ -11,9 +11,65 @@ use petri::openvmm::OpenVmmPetriBackend;
 use vmm_test_macros::openvmm_test;
 use vmm_test_macros::openvmm_test_no_agent;
 
+/// Convenience method to get a child property of an inspect node. e.g. if you
+/// have a node like: `{ "save_restore_supported": true }`, then you can call
+/// `child("save_restore_supported", &node, ||(v) { ... })` to get the value
+/// (which would be `true` in this case).
+fn child<T>(
+    node: &inspect::Node,
+    name: &str,
+    f: impl FnOnce(&inspect::ValueKind) -> anyhow::Result<T>,
+) -> anyhow::Result<T> {
+    let children = match node {
+        inspect::Node::Dir(children) => children,
+        _ => anyhow::bail!("Expected directory (looking for `{}`)", name),
+    };
+
+    let child = children
+        .iter()
+        .find(|c| c.name == name)
+        .with_context(|| format!("missing child `{}`", name))?;
+
+    match &child.node {
+        inspect::Node::Value(value) => f(&value.kind),
+        _ => anyhow::bail!("Expected `{}` to be a value", name),
+    }
+}
+
+fn bool_child(node: &inspect::Node, name: &str) -> anyhow::Result<bool> {
+    child(node, name, |v| match v {
+        inspect::ValueKind::Bool(b) => Ok(*b),
+        _ => anyhow::bail!("Expected `{}` to be a bool", name),
+    })
+}
+
+fn unsigned_child(node: &inspect::Node, name: &str) -> anyhow::Result<u64> {
+    child(node, name, |v| match v {
+        inspect::ValueKind::Unsigned(u) => Ok(*u),
+        _ => anyhow::bail!("Expected `{}` to be an unsigned integer", name),
+    })
+}
+
+struct ExpectedNvmeDeviceProperties {
+    save_restore_supported: bool,
+    qsize: u64,
+    nvme_keepalive: bool,
+}
+
+/// Helper to run a scenario where we boot an OpenHCL UEFI VM with a NVME
+/// disk assigned to VTL2.
+///
+/// Validates that the VTL2 NVMe driver is working as expected by comparing
+/// the inspect properties of the NVMe device against the supplied expected
+/// properties.
+///
+/// If `props` is `None`, then we skip validating the properties. (This is useful
+/// at this moment for while we finish developing NVMe keepalive, which is needed
+/// to get the devices to work as expected.)
 async fn nvme_relay_test_core(
     config: PetriVmBuilder<OpenVmmPetriBackend>,
     openhcl_cmdline: &str,
+    props: Option<ExpectedNvmeDeviceProperties>,
 ) -> Result<(), anyhow::Error> {
     let (vm, agent) = config
         .with_openhcl_command_line(openhcl_cmdline)
@@ -25,6 +81,51 @@ async fn nvme_relay_test_core(
         .run()
         .await?;
 
+    let devices_node = vm.inspect_openhcl("vm/nvme/devices", None, None).await?;
+    tracing::info!(devices = %devices_node.json(), "NVMe devices");
+    if let inspect::Node::Dir(devices) = devices_node {
+        // Inspect the NVMe devices
+        assert!(!devices.is_empty(), "Expected at least one NVMe device");
+        // For now, assume that the first device is just the one we expect.
+        // But, [`PARAVISOR_BOOT_NVME_INSTANCE`] contains the PCI instance.
+        let props = props.inspect(|p| {
+            assert_eq!(
+                p.save_restore_supported,
+                bool_child(&devices[0].node, "save_restore_supported")
+                    .expect("save_restore_supported")
+            )
+        });
+
+        // Get the guts of the device...
+        let device_details = vm
+            .inspect_openhcl(
+                [
+                    "vm/nvme/devices".to_owned(),
+                    devices[0].name.clone(),
+                    "driver/driver".to_owned(),
+                ]
+                .join("/")
+                .as_str(),
+                None,
+                None,
+            )
+            .await?;
+        let props = props.inspect(|p| {
+            assert_eq!(
+                p.qsize,
+                unsigned_child(&device_details, "qsize").expect("qsize")
+            )
+        });
+        props.inspect(|p| {
+            assert_eq!(
+                p.nvme_keepalive,
+                bool_child(&device_details, "nvme_keepalive").expect("nvme_keepalive")
+            )
+        });
+    } else {
+        anyhow::bail!("Not expected: `vm/nvme/devices` is not a directory");
+    }
+
     agent.power_off().await?;
     vm.wait_for_clean_teardown().await?;
 
@@ -35,7 +136,7 @@ async fn nvme_relay_test_core(
 /// linux, with vmbus relay. This should expose a disk to VTL0 via vmbus.
 #[openvmm_test(openhcl_uefi_x64[nvme](vhd(ubuntu_2204_server_x64)))]
 async fn nvme_relay(config: PetriVmBuilder<OpenVmmPetriBackend>) -> Result<(), anyhow::Error> {
-    nvme_relay_test_core(config, "").await
+    nvme_relay_test_core(config, "", None).await
 }
 
 /// Test an OpenHCL uefi VM with a NVME disk assigned to VTL2 that boots
@@ -46,7 +147,7 @@ async fn nvme_relay(config: PetriVmBuilder<OpenVmmPetriBackend>) -> Result<(), a
 async fn nvme_relay_shared_pool(
     config: PetriVmBuilder<OpenVmmPetriBackend>,
 ) -> Result<(), anyhow::Error> {
-    nvme_relay_test_core(config, "OPENHCL_ENABLE_SHARED_VISIBILITY_POOL=1").await
+    nvme_relay_test_core(config, "OPENHCL_ENABLE_SHARED_VISIBILITY_POOL=1", None).await
 }
 
 /// Test an OpenHCL uefi VM with a NVME disk assigned to VTL2 that boots
@@ -58,7 +159,16 @@ async fn nvme_relay_private_pool(
     config: PetriVmBuilder<OpenVmmPetriBackend>,
 ) -> Result<(), anyhow::Error> {
     // Number of pages to reserve as a private pool.
-    nvme_relay_test_core(config, "OPENHCL_ENABLE_VTL2_GPA_POOL=512").await
+    nvme_relay_test_core(
+        config,
+        "OPENHCL_ENABLE_VTL2_GPA_POOL=512",
+        Some(ExpectedNvmeDeviceProperties {
+            save_restore_supported: true,
+            qsize: 64,
+            nvme_keepalive: false,
+        }),
+    )
+    .await
 }
 
 /// Boot the UEFI firmware, with a VTL2 range automatically configured by
