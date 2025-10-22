f36d907d2347cc07760cf5f628b715d27a40b659
Steven Malis <stevenmalis@microsoft.com>
2025-10-22 12:12:49 -0400

    Undo pal_uring

--- support/pal/pal_uring/src/lib.rs
+++ support/pal/pal_uring/src/lib.rs
@@ -1,12 +1,14 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+#![cfg_attr(not(target_os = "linux"), expect(missing_docs))]
+#![cfg(target_os = "linux")]
+
 //! [`pal_async`] support for the Linux kernel's io_uring.
 //!
 //! Historically, this crate included a per-CPU thread pool implementation. Some
 //! vestigates remain in naming and elsewhere. TODO: clean this up.
 
-#![cfg(target_os = "linux")]
 // UNSAFETY: This module uses unsafe code to interact with the io_uring kernel
 // interface.
 #![expect(unsafe_code)]
