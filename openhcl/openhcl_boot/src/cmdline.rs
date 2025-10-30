5fef4167a46ed83f0a73c3995cceb32efd2d4e0b
Matt LaFayette (Kurjanowicz) <mattkur@microsoft.com>
2025-10-30 14:11:38 +0000

    sigh, override override override

--- openhcl/openhcl_boot/src/cmdline.rs
+++ openhcl/openhcl_boot/src/cmdline.rs
@@ -15,6 +15,11 @@ use underhill_confidentiality::OPENHCL_CONFIDENTIAL_DEBUG_ENV_VAR_NAME;
 /// * `<num_pages>`: Explicitly specify the size of the VTL2 GPA pool.
 ///
 /// See `Vtl2GpaPoolConfig` for more details.
+const IGVM_VTL2_GPA_POOL_CONFIG: &str = "OPENHCL_IGVM_VTL2_GPA_POOL_CONFIG=";
+
+/// Test-legacy/test-compat override for `OPENHCL_IGVM_VTL2_GPA_POOL_CONFIG`.
+/// (otherwise, tests cannot modify the VTL2 GPA pool config different from what
+/// may be in the manifest).
 const ENABLE_VTL2_GPA_POOL: &str = "OPENHCL_ENABLE_VTL2_GPA_POOL=";
 
 /// Options controlling sidecar.
@@ -58,6 +63,26 @@ pub enum Vtl2GpaPoolConfig {
     Pages(u64),
 }
 
+impl<S: AsRef<str>> From<S> for Vtl2GpaPoolConfig {
+    fn from(arg: S) -> Self {
+        match arg.as_ref() {
+            "debug" => Vtl2GpaPoolConfig::Heuristics(Vtl2GpaPoolLookupTable::Debug),
+            "release" => Vtl2GpaPoolConfig::Heuristics(Vtl2GpaPoolLookupTable::Release),
+            "off" => Vtl2GpaPoolConfig::Off,
+            _ => {
+                let num = arg.as_ref().parse::<u64>().unwrap_or(0);
+                // A size of 0 or failure to parse is treated as disabling
+                // the pool.
+                if num == 0 {
+                    Vtl2GpaPoolConfig::Off
+                } else {
+                    Vtl2GpaPoolConfig::Pages(num)
+                }
+            }
+        }
+    }
+}
+
 #[derive(Debug, PartialEq)]
 pub struct BootCommandLineOptions {
     pub confidential_debug: bool,
@@ -82,29 +107,22 @@ impl BootCommandLineOptions {
 impl BootCommandLineOptions {
     /// Parse arguments from a command line.
     pub fn parse(&mut self, cmdline: &str) {
+        let mut override_vtl2_gpa_pool: Option<Vtl2GpaPoolConfig> = None;
         for arg in cmdline.split_whitespace() {
             if arg.starts_with(OPENHCL_CONFIDENTIAL_DEBUG_ENV_VAR_NAME) {
                 let arg = arg.split_once('=').map(|(_, arg)| arg);
                 if arg.is_some_and(|a| a != "0") {
                     self.confidential_debug = true;
                 }
+            } else if arg.starts_with(IGVM_VTL2_GPA_POOL_CONFIG) {
+                if let Some((_, arg)) = arg.split_once('=') {
+                    self.enable_vtl2_gpa_pool = Vtl2GpaPoolConfig::from(arg);
+                } else {
+                    log!("WARNING: Missing value for IGVM_VTL2_GPA_POOL_CONFIG argument");
+                }
             } else if arg.starts_with(ENABLE_VTL2_GPA_POOL) {
                 if let Some((_, arg)) = arg.split_once('=') {
-                    self.enable_vtl2_gpa_pool = match arg {
-                        "debug" => Vtl2GpaPoolConfig::Heuristics(Vtl2GpaPoolLookupTable::Debug),
-                        "release" => Vtl2GpaPoolConfig::Heuristics(Vtl2GpaPoolLookupTable::Release),
-                        "off" => Vtl2GpaPoolConfig::Off,
-                        _ => {
-                            let num = arg.parse::<u64>().unwrap_or(0);
-                            // A size of 0 or failure to parse is treated as disabling
-                            // the pool.
-                            if num == 0 {
-                                Vtl2GpaPoolConfig::Off
-                            } else {
-                                Vtl2GpaPoolConfig::Pages(num)
-                            }
-                        }
-                    }
+                    override_vtl2_gpa_pool = Some(Vtl2GpaPoolConfig::from(arg));
                 } else {
                     log!("WARNING: Missing value for ENABLE_VTL2_GPA_POOL argument");
                 }
@@ -126,6 +144,14 @@ impl BootCommandLineOptions {
                 }
             }
         }
+
+        if let Some(override_config) = override_vtl2_gpa_pool {
+            self.enable_vtl2_gpa_pool = override_config;
+            log!(
+                "INFO: Overriding VTL2 GPA pool config to {:?} from command line",
+                override_config
+            );
+        }
     }
 }
 
@@ -141,57 +167,53 @@ mod tests {
 
     #[test]
     fn test_vtl2_gpa_pool_parsing() {
-        assert_eq!(
-            parse_boot_command_line("OPENHCL_ENABLE_VTL2_GPA_POOL=1"),
-            BootCommandLineOptions {
-                enable_vtl2_gpa_pool: Vtl2GpaPoolConfig::Pages(1),
-                ..BootCommandLineOptions::new()
-            }
-        );
-        assert_eq!(
-            parse_boot_command_line("OPENHCL_ENABLE_VTL2_GPA_POOL=0"),
-            BootCommandLineOptions {
-                enable_vtl2_gpa_pool: Vtl2GpaPoolConfig::Off,
-                ..BootCommandLineOptions::new()
-            }
-        );
-        assert_eq!(
-            parse_boot_command_line("OPENHCL_ENABLE_VTL2_GPA_POOL=asdf"),
-            BootCommandLineOptions {
-                enable_vtl2_gpa_pool: Vtl2GpaPoolConfig::Off,
-                ..BootCommandLineOptions::new()
-            }
-        );
-        assert_eq!(
-            parse_boot_command_line("OPENHCL_ENABLE_VTL2_GPA_POOL=512"),
-            BootCommandLineOptions {
-                enable_vtl2_gpa_pool: Vtl2GpaPoolConfig::Pages(512),
-                ..BootCommandLineOptions::new()
-            }
-        );
-        assert_eq!(
-            parse_boot_command_line("OPENHCL_ENABLE_VTL2_GPA_POOL=off"),
-            BootCommandLineOptions {
-                enable_vtl2_gpa_pool: Vtl2GpaPoolConfig::Off,
-                ..BootCommandLineOptions::new()
-            }
-        );
-        assert_eq!(
-            parse_boot_command_line("OPENHCL_ENABLE_VTL2_GPA_POOL=debug"),
-            BootCommandLineOptions {
-                enable_vtl2_gpa_pool: Vtl2GpaPoolConfig::Heuristics(Vtl2GpaPoolLookupTable::Debug),
-                ..BootCommandLineOptions::new()
-            }
-        );
-        assert_eq!(
-            parse_boot_command_line("OPENHCL_ENABLE_VTL2_GPA_POOL=release"),
-            BootCommandLineOptions {
-                enable_vtl2_gpa_pool: Vtl2GpaPoolConfig::Heuristics(
-                    Vtl2GpaPoolLookupTable::Release
-                ),
-                ..BootCommandLineOptions::new()
-            }
-        );
+        for (cmdline, expected) in [
+            (
+                // default
+                "",
+                Vtl2GpaPoolConfig::Heuristics(Vtl2GpaPoolLookupTable::Release),
+            ),
+            (
+                "OPENHCL_IGVM_VTL2_GPA_POOL_CONFIG=1",
+                Vtl2GpaPoolConfig::Pages(1),
+            ),
+            (
+                "OPENHCL_IGVM_VTL2_GPA_POOL_CONFIG=0",
+                Vtl2GpaPoolConfig::Off,
+            ),
+            (
+                "OPENHCL_IGVM_VTL2_GPA_POOL_CONFIG=asdf",
+                Vtl2GpaPoolConfig::Off,
+            ),
+            (
+                "OPENHCL_IGVM_VTL2_GPA_POOL_CONFIG=512",
+                Vtl2GpaPoolConfig::Pages(512),
+            ),
+            (
+                "OPENHCL_IGVM_VTL2_GPA_POOL_CONFIG=off",
+                Vtl2GpaPoolConfig::Off,
+            ),
+            (
+                "OPENHCL_IGVM_VTL2_GPA_POOL_CONFIG=debug",
+                Vtl2GpaPoolConfig::Heuristics(Vtl2GpaPoolLookupTable::Debug),
+            ),
+            (
+                "OPENHCL_IGVM_VTL2_GPA_POOL_CONFIG=release",
+                Vtl2GpaPoolConfig::Heuristics(Vtl2GpaPoolLookupTable::Release),
+            ),
+            (
+                // OPENHCL_ENABLE_VTL2_GPA_POOL= takes precedence over OPENHCL_IGVM_VTL2_GPA_POOL_CONFIG=
+                "OPENHCL_IGVM_VTL2_GPA_POOL_CONFIG=release OPENHCL_ENABLE_VTL2_GPA_POOL=debug",
+                Vtl2GpaPoolConfig::Heuristics(Vtl2GpaPoolLookupTable::Debug),
+            ),
+        ] {
+            assert_eq!(
+                parse_boot_command_line(cmdline).enable_vtl2_gpa_pool,
+                expected,
+                "Failed parsing VTL2 GPA pool config from command line: {}",
+                cmdline
+            );
+        }
     }
 
     #[test]
