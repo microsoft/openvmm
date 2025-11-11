3e38e0c7ed29e3a14e49d9f8dc9cc0bfdb5f13b6
Matt LaFayette <mattkur@microsoft.com>
2025-11-11 21:10:56 +0000

    nvme_driver: format nvme errors to be more verbose

--- vm/devices/storage/disk_nvme/nvme_driver/src/queue_pair.rs
+++ vm/devices/storage/disk_nvme/nvme_driver/src/queue_pair.rs
@@ -480,14 +480,20 @@ impl std::error::Error for NvmeError {}
 impl std::fmt::Display for NvmeError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self.0.status_code_type() {
-            spec::StatusCodeType::GENERIC => write!(f, "general error {:#x?}", self.0),
+            spec::StatusCodeType::GENERIC => write!(f, "NVMe SCT general error, SC: {:#x?}", self.0),
             spec::StatusCodeType::COMMAND_SPECIFIC => {
-                write!(f, "command-specific error {:#x?}", self.0)
+                write!(f, "NVMe SCT command-specific error, SC: {:#x?}", self.0)
             }
             spec::StatusCodeType::MEDIA_ERROR => {
-                write!(f, "media error {:#x?}", self.0)
+                write!(f, "NVMe SCT media error, SC: {:#x?}", self.0)
             }
-            _ => write!(f, "{:#x?}", self.0),
+            spec::StatusCodeType::PATH_RELATED => {
+                write!(f, "NVMe SCT path-related error, SC: {:#x?}", self.0)
+            }
+            spec::StatusCodeType::VENDOR_SPECIFIC => {
+                write!(f, "NVMe SCT vendor-specific error, SC: {:#x?}", self.0)
+            }
+            _ => write!(f, "(other NVMe SCT), {:#x?}", self.0),
         }
     }
 }
