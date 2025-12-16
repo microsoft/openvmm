7c0f53fbe234bc452514feb758eab6aedfb72a6a
Steven Malis <137308034+smalis-msft@users.noreply.github.com>
2025-12-16 13:11:55 -0500

    Bring this one back

--- openvmm/hvlite_core/src/worker/dispatch.rs
+++ openvmm/hvlite_core/src/worker/dispatch.rs
@@ -786,6 +786,7 @@ impl InitializedVm {
         }
     }
 
+    #[allow(dead_code)]
     async fn new_with_hypervisor<P, H>(
         driver_source: VmTaskDriverSource,
         hypervisor: &mut H,
