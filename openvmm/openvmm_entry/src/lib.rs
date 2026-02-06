ca3e843aabe78efeacbc22e66f15a92da4828675
maheeraeron <maheeraeron@microsoft.com>
2026-02-06 22:12:29 +0000

    Finished plumbing to petri and openvmm and add tests

--- openvmm/openvmm_entry/src/lib.rs
+++ openvmm/openvmm_entry/src/lib.rs
@@ -896,6 +896,7 @@ async fn vm_config_from_command_line(
             }),
             default_boot_always_attempt: opt.default_boot_always_attempt,
             bios_guid,
+            azi_hsm_enabled: opt.azi_hsm_enabled,
         };
     } else {
         // Linux Direct
@@ -1062,7 +1063,7 @@ async fn vm_config_from_command_line(
                         }
                     },
                     hv_sint_enabled: false,
-                    azi_hsm_enabled: false,
+                    azi_hsm_enabled: opt.azi_hsm_enabled,
                 }
                 .into_resource(),
             ),
