41adb65ba1fd1d70d14a04da458b55122ede4112
Maheer Aeron <maheeraeron@microsoft.com>
2026-02-18 15:37:28 -0800

    openhcl: enable the battery for isolated VMs (#2798)
    Work Item: MSFT#59988631
    
    Allows the battery for OpenHCL Isolated VM configurations, which matches
    our legacy HCL today

--- openhcl/underhill_core/src/worker.rs
+++ openhcl/underhill_core/src/worker.rs
@@ -3652,7 +3652,6 @@ fn validate_isolated_configuration(dps: &DevicePlatformSettings) -> Result<(), a
         vpci_boot_enabled: _,
 
         // Validated below
-        battery_enabled,
         processor_idle_enabled,
         firmware_debugging_enabled,
         hibernation_enabled,
@@ -3699,6 +3698,7 @@ fn validate_isolated_configuration(dps: &DevicePlatformSettings) -> Result<(), a
         management_vtl_features: _,
         hv_sint_enabled: _,
         azi_hsm_enabled: _,
+        battery_enabled: _, // TODO: Add this to attestation later
     } = &dps.general;
 
     if *hibernation_enabled {
@@ -3709,9 +3709,6 @@ fn validate_isolated_configuration(dps: &DevicePlatformSettings) -> Result<(), a
     }
     if *secure_boot_enabled && *firmware_debugging_enabled {
         anyhow::bail!("secure boot and firmware debugging are mutually exclusive");
-    }
-    if *battery_enabled {
-        anyhow::bail!("battery is not supported");
     }
     if *legacy_memory_map {
         anyhow::bail!("legacy memory map is not supported");
