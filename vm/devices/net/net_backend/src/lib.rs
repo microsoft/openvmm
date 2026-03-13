b3b5e00b1ae622a72ebc2dc886731d222978552a
Ben Lewis <Ben.Lewis@microsoft.com>
2026-03-13 14:51:17 -0700

    Add a deadline to disconnecting endpoints, and a trace on failure.

--- vm/devices/net/net_backend/src/lib.rs
+++ vm/devices/net/net_backend/src/lib.rs
@@ -591,6 +591,7 @@ impl Endpoint for DisconnectableEndpoint {
             match receive_update.next().await {
                 Some(m) => Message::DisconnectableEndpointUpdate(m),
                 None => {
+                    tracing::warn!("Failed to receive an update; receiver disconnected.");
                     pending::<()>().await;
                     unreachable!()
                 }
