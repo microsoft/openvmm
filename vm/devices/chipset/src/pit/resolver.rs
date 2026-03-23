4625ed834f323d5561de41c4d633c869c02cb431
Kyle Moorhead <kylemoorhead@microsoft.com>
2026-03-23 15:52:43 +0000

    Comment cleanup for PIT wiring

--- vm/devices/chipset/src/pit/resolver.rs
+++ vm/devices/chipset/src/pit/resolver.rs
@@ -33,7 +33,6 @@ impl AsyncResolveResource<ChipsetDeviceHandleKind, PitDeviceHandle> for PitResol
         _resource: PitDeviceHandle,
         input: ResolveChipsetDeviceHandleParams<'_>,
     ) -> Result<Self::Output, Self::Error> {
-        // Preserve legacy wiring used by base_chipset before PIT resourcification.
         let interrupt = input.configure.new_line(IRQ_LINE_SET, "timer0", 2);
         let vmtime = input.vmtime.access("pit");
         Ok(PitDevice::new(interrupt, vmtime).into())
