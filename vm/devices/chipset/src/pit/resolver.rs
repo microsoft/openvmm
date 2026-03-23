585eb2555853be678d4f3411a6749f3290394b8f
Kyle Moorhead <kylemoorhead@microsoft.com>
2026-03-23 17:56:35 +0000

    Update PIT resolver to use non-asynchronous ResolveResource trait

--- vm/devices/chipset/src/pit/resolver.rs
+++ vm/devices/chipset/src/pit/resolver.rs
@@ -4,32 +4,28 @@
 //! Resource resolver for the PIT (Programmable Interval Timer) chipset device.
 
 use super::PitDevice;
-use async_trait::async_trait;
 use chipset_device_resources::IRQ_LINE_SET;
 use chipset_device_resources::ResolveChipsetDeviceHandleParams;
 use chipset_device_resources::ResolvedChipsetDevice;
 use chipset_resources::pit::PitDeviceHandle;
-use vm_resource::AsyncResolveResource;
-use vm_resource::ResourceResolver;
-use vm_resource::declare_static_async_resolver;
+use vm_resource::ResolveResource;
+use vm_resource::declare_static_resolver;
 use vm_resource::kind::ChipsetDeviceHandleKind;
 
 /// A resolver for PIT devices.
 pub struct PitResolver;
 
-declare_static_async_resolver! {
+declare_static_resolver! {
     PitResolver,
     (ChipsetDeviceHandleKind, PitDeviceHandle),
 }
 
-#[async_trait]
-impl AsyncResolveResource<ChipsetDeviceHandleKind, PitDeviceHandle> for PitResolver {
+impl ResolveResource<ChipsetDeviceHandleKind, PitDeviceHandle> for PitResolver {
     type Output = ResolvedChipsetDevice;
     type Error = std::convert::Infallible;
 
-    async fn resolve(
+    fn resolve(
         &self,
-        _resolver: &ResourceResolver,
         _resource: PitDeviceHandle,
         input: ResolveChipsetDeviceHandleParams<'_>,
     ) -> Result<Self::Output, Self::Error> {
