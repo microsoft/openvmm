04b2e1f62e85d0c10daec35430f55617d01ea32c
John Starks <jostarks@microsoft.com>
2026-04-01 00:13:20 +0000

    comment

--- vm/devices/virtio/virtio/src/transport/mod.rs
+++ vm/devices/virtio/virtio/src/transport/mod.rs
@@ -13,6 +13,10 @@ mod task;
 
 /// Validate that a queue size returned by a device is acceptable for use by a
 /// transport: non-zero, power of two, and within [`MAX_QUEUE_SIZE`].
+///
+/// Note that only split queues require a power of two size, but since we don't
+/// know which type of queue the guest will select, the default queue size must
+/// be a power of two to be compatible with both packed and split queues.
 fn validate_queue_size(queue_index: u16, size: u16) -> io::Result<()> {
     if size == 0 || !size.is_power_of_two() || size > MAX_QUEUE_SIZE {
         return Err(io::Error::new(
