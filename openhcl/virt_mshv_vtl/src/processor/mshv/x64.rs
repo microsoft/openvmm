69b8ff3d8315b020fa6a8948ae592106fd4bbfee
John Starks <jostarks@microsoft.com>
2026-04-15 15:54:18 +0000

    woah

--- openhcl/virt_mshv_vtl/src/processor/mshv/x64.rs
+++ openhcl/virt_mshv_vtl/src/processor/mshv/x64.rs
@@ -1091,10 +1091,10 @@ impl<T: CpuIo> EmulatorSupport for UhEmulationState<'_, '_, T, HypervisorBackedX
     }
 
     fn set_gp(&mut self, reg: x86emu::Gp, v: u64) {
-        if reg == x86emu::Gp::RSP {
-            self.cache.rsp = v;
+        match reg {
+            x86emu::Gp::RSP => self.cache.rsp = v,
+            _ => self.vp.runner.cpu_context_mut().gps_no_rsp[reg as usize] = v,
         }
-        self.vp.runner.cpu_context_mut().gps_no_rsp[reg as usize] = v;
     }
 
     fn xmm(&mut self, index: usize) -> u128 {
