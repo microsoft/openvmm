a93a0afe097be00353c674711a7ace41004d399c
Steven Malis <stevenmalis@microsoft.com>
2026-04-20 12:35:34 -0400

    Fix comment

--- openhcl/openhcl_boot/build.rs
+++ openhcl/openhcl_boot/build.rs
@@ -9,8 +9,10 @@ fn main() {
 
     // By default the sha2 crate uses cpu feature detection which on x86_64 uses the
     // cpuid instruction. Executing cpuid in an SNP CVM would require implementing an
-    // exception handler. Using the force-soft feature flag enables a software
-    // implementation of the hashing algorithms that does not use cpuid.
+    // exception handler. Using the "soft" configuration flag forces a software
+    // implementation of the hashing algorithms that does not use cpuid. The
+    // "compact" flag disables loop unrolling, providing a smaller code size at
+    // the cost of performance.
     println!("cargo:rustc-cfg=sha2_backend=\"soft\"");
     println!("cargo:rustc-cfg=sha2_backend_soft=\"compact\"");
 
