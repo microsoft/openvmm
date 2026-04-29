9e068b06d92e9847f2ce447e0171d7ec5f6916fc
maheeraeron <maheeraeron@microsoft.com>
2026-04-29 23:16:52 +0000

    Update nix

--- nix/uefi_mu_msvm.nix
+++ nix/uefi_mu_msvm.nix
@@ -14,11 +14,11 @@ let
 
 in stdenv.mkDerivation {
   pname = "uefi-mu-msvm-${archToolchain}";
-  version = "26.0.0";
+  version = "26.0.2";
 
   src = fetchzip {
     url =
-      "https://github.com/microsoft/mu_msvm/releases/download/v26.0.0/RELEASE-${archToolchain}-artifacts.tar.gz";
+      "https://github.com/microsoft/mu_msvm/releases/download/v26.0.2/RELEASE-${archToolchain}-artifacts.tar.gz";
     stripRoot = false;
     inherit hash;
   };
