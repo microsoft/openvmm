88aed69b618f45164f6bbd06a17254c4ff4cf0f4
Guramrit Singh <gurasingh@microsoft.com>
2026-05-06 17:57:59 -0700

    Revert "For a local build"
    This reverts commit 4efda83fd3f175c3e606a49f635f61ff793d2c77.

--- openhcl/underhill_core/src/nvme_manager/mod.rs
+++ openhcl/underhill_core/src/nvme_manager/mod.rs
@@ -73,7 +73,7 @@ const KEEPALIVE_INCOMPATIBLE_VENDOR_ID: &str = "0x1414";
 
 /// PCI device ID, as it appears in the sysfs `device` file (e.g. `0x0100`),
 /// for NVMe devices that are incompatible with keepalive.
-const KEEPALIVE_INCOMPATIBLE_DEVICE_ID: &str = "0x0000";
+const KEEPALIVE_INCOMPATIBLE_DEVICE_ID: &str = "0xb111";
 
 #[derive(Debug, Error)]
 pub enum NvmeSpawnerError {
