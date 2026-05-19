50753234e1825aba32a8d42d2d986c658376afcc
Steven Malis <stevenmalis@microsoft.com>
2026-05-19 12:25:29 -0400

    crypto: Add a native feature flag

--- support/crypto/build.rs
+++ support/crypto/build.rs
@@ -4,6 +4,7 @@
 #![expect(missing_docs)]
 
 fn main() {
+    println!("cargo::rerun-if-env-changed=CARGO_FEATURE_NATIVE");
     println!("cargo::rerun-if-env-changed=CARGO_FEATURE_OPENSSL");
     println!("cargo::rerun-if-env-changed=CARGO_FEATURE_RUST");
     println!("cargo::rerun-if-env-changed=CARGO_FEATURE_SYMCRYPT");
@@ -15,14 +16,15 @@ fn main() {
     println!("cargo::rustc-check-cfg=cfg(rust)");
     println!("cargo::rustc-check-cfg=cfg(symcrypt)");
 
+    let native = std::env::var_os("CARGO_FEATURE_NATIVE").is_some();
     let openssl = std::env::var_os("CARGO_FEATURE_OPENSSL").is_some();
     let rust = std::env::var_os("CARGO_FEATURE_RUST").is_some();
     let symcrypt = std::env::var_os("CARGO_FEATURE_SYMCRYPT").is_some();
     let vendored = std::env::var_os("CARGO_FEATURE_VENDORED").is_some();
     let linux = std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "linux";
 
-    let all_features = openssl && rust && symcrypt && vendored;
-    let backend_count = openssl as u8 + rust as u8 + symcrypt as u8;
+    let all_features = native && openssl && rust && symcrypt && vendored;
+    let backend_count = native as u8 + openssl as u8 + rust as u8 + symcrypt as u8;
 
     // If we see multiple backends enabled that's an error. However if we see every
     // backend, and vendoring, enabled, it's likely we're in an --all-features session.
@@ -30,7 +32,9 @@ fn main() {
     // platform defaults.
     if backend_count > 1 && !all_features {
         panic!("Multiple crypto backends enabled, but only one can be selected");
-    } else if backend_count == 0 || all_features {
+    } else if backend_count == 0 {
+        panic!("No crypto backend enabled, but one must be selected");
+    } else if native || all_features {
         // Default to openssl on linux, the dependencies are also marked
         // non-optional and there is no native backend available
         if linux {
