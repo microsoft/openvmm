86b78d0809346a70e3b6e61f48c1c7d3a0e64a33
Trevor Jones <jonestrevor@microsoft.com>
2026-06-23 23:38:24 -0700

    balance

--- flowey/flowey_hvlite/src/pipelines/checkin_gates.rs
+++ flowey/flowey_hvlite/src/pipelines/checkin_gates.rs
@@ -435,7 +435,7 @@ impl IntoPipeline for CheckinGatesCli {
                 FlowArch::X86_64,
                 "build artifacts (shared VMM tests) [linux]",
             )
-            .gh_set_pool(gh_pools::default_linux())
+            .gh_set_pool(gh_pools::linux_intel_v6_1es())
             .ado_set_pool(ado_pools::default_linux());
         for (
             arch,
@@ -1151,7 +1151,7 @@ impl IntoPipeline for CheckinGatesCli {
             ClippyUnitTestJobParams {
                 platform: FlowPlatform::Windows,
                 arch: FlowArch::X86_64,
-                gh_pool: gh_pools::windows_amd_v6_1es(),
+                gh_pool: gh_pools::windows_intel_v6_1es(),
                 ado_pool: Some(ado_pools::windows_amd_1es()),
                 clippy_targets: Some((
                     "x64-windows",
@@ -1165,7 +1165,7 @@ impl IntoPipeline for CheckinGatesCli {
             ClippyUnitTestJobParams {
                 platform: FlowPlatform::Linux(FlowPlatformLinuxDistro::Ubuntu),
                 arch: FlowArch::X86_64,
-                gh_pool: gh_pools::linux_amd_v7_1es(),
+                gh_pool: gh_pools::linux_intel_v6_1es(),
                 ado_pool: Some(ado_pools::linux_amd_1es()),
                 clippy_targets: if quick_check_job.is_some() {
                     // quick check already ran clippy for x64-linux;
@@ -1185,7 +1185,7 @@ impl IntoPipeline for CheckinGatesCli {
             ClippyUnitTestJobParams {
                 platform: FlowPlatform::Linux(FlowPlatformLinuxDistro::Ubuntu),
                 arch: FlowArch::X86_64,
-                gh_pool: gh_pools::linux_amd_v7_1es(),
+                gh_pool: gh_pools::linux_intel_v6_1es(),
                 ado_pool: Some(ado_pools::linux_amd_1es()),
                 clippy_targets: Some((
                     "x64-linux-musl, misc nostd",
@@ -1210,7 +1210,7 @@ impl IntoPipeline for CheckinGatesCli {
             ClippyUnitTestJobParams {
                 platform: FlowPlatform::Linux(FlowPlatformLinuxDistro::Ubuntu),
                 arch: FlowArch::Aarch64,
-                gh_pool: gh_pools::linux_arm_v6_1es(),
+                gh_pool: gh_pools::linux_arm_v5_1es(),
                 ado_pool: None,
                 clippy_targets: Some((
                     "aarch64-linux",
@@ -1224,7 +1224,7 @@ impl IntoPipeline for CheckinGatesCli {
             ClippyUnitTestJobParams {
                 platform: FlowPlatform::Linux(FlowPlatformLinuxDistro::Ubuntu),
                 arch: FlowArch::Aarch64,
-                gh_pool: gh_pools::linux_arm_v6_1es(),
+                gh_pool: gh_pools::linux_arm_v5_1es(),
                 ado_pool: None,
                 clippy_targets: Some((
                     "aarch64-linux-musl, misc nostd",
@@ -1560,8 +1560,7 @@ impl IntoPipeline for CheckinGatesCli {
             VmmTestJobParams {
                 platform: FlowPlatform::Linux(FlowPlatformLinuxDistro::Ubuntu),
                 arch: FlowArch::X86_64,
-                // use a separate pool for vmm tests to maximize quota use
-                gh_pool: gh_pools::linux_amd_v7_1es_2(),
+                gh_pool: gh_pools::linux_amd_v7_1es(),
                 ado_pool: Some(ado_pools::linux_amd_1es()),
                 label: "x64-linux-amd-kvm",
                 target: CommonTriple::X86_64_LINUX_GNU,
--- flowey/flowey_hvlite/src/pipelines_shared/gh_pools.rs
+++ flowey/flowey_hvlite/src/pipelines_shared/gh_pools.rs
@@ -7,9 +7,9 @@ use flowey::pipeline::prelude::*;
 
 pub const AMD_V6_POOL_1ES: &str = "openvmm-gh-amd-westus3-v6";
 pub const AMD_V7_POOL_1ES: &str = "openvmm-gh-amd-westus3-v7";
-pub const AMD_V7_POOL_1ES_2: &str = "openvmm-gh-amd-westus2-v7";
 pub const INTEL_V5_POOL_1ES: &str = "openvmm-gh-intel-westus3";
 pub const INTEL_V6_POOL_1ES: &str = "openvmm-gh-intel-westus3-v6";
+pub const ARM_V5_POOL_1ES: &str = "openvmm-gh-arm-westus2";
 pub const ARM_V6_POOL_1ES: &str = "openvmm-gh-arm-westus3";
 
 pub const WINDOWS_IMAGE_AMD64: &str = "win-amd64-v2";
@@ -26,6 +26,10 @@ fn gh_pool_with_image_1es(pool: &str, image: &str) -> GhRunner {
     ])
 }
 
+pub fn windows_arm_v6_1es() -> GhRunner {
+    gh_pool_with_image_1es(ARM_V6_POOL_1ES, WINDOWS_IMAGE_ARM64)
+}
+
 pub fn windows_amd_v6_1es() -> GhRunner {
     gh_pool_with_image_1es(AMD_V6_POOL_1ES, WINDOWS_IMAGE_AMD64)
 }
@@ -34,20 +38,16 @@ pub fn windows_intel_v6_1es() -> GhRunner {
     gh_pool_with_image_1es(INTEL_V6_POOL_1ES, WINDOWS_IMAGE_AMD64)
 }
 
-pub fn windows_arm_v6_1es() -> GhRunner {
-    gh_pool_with_image_1es(ARM_V6_POOL_1ES, WINDOWS_IMAGE_ARM64)
+pub fn linux_arm_v5_1es() -> GhRunner {
+    gh_pool_with_image_1es(ARM_V6_POOL_1ES, LINUX_IMAGE_ARM64)
 }
 
-pub fn linux_arm_v6_1es() -> GhRunner {
-    gh_pool_with_image_1es(ARM_V6_POOL_1ES, LINUX_IMAGE_ARM64)
+pub fn linux_intel_v6_1es() -> GhRunner {
+    gh_pool_with_image_1es(INTEL_V6_POOL_1ES, LINUX_IMAGE_AMD64)
 }
 
 pub fn linux_amd_v7_1es() -> GhRunner {
     gh_pool_with_image_1es(AMD_V7_POOL_1ES, LINUX_IMAGE_AMD64)
-}
-
-pub fn linux_amd_v7_1es_2() -> GhRunner {
-    gh_pool_with_image_1es(AMD_V7_POOL_1ES_2, LINUX_IMAGE_AMD64)
 }
 
 pub fn linux_mshv_intel_v5_1es() -> GhRunner {
