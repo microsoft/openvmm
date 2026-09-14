c8900fb3eec3e2b542da545176c31276b52baa18
Henry Wang <wanghenry@microsoft.com>
2026-09-14 13:06:44 -0700

    test

--- opentmk/opentmk_executor/src/main.rs
+++ opentmk/opentmk_executor/src/main.rs
@@ -7,76 +7,63 @@
 //! framework used to accept custom-crafted program consisting of an encoded
 //! series of functions that would invoke specific functions into this OS
 
+#![cfg(any(test, target_os = "uefi"))]
 #![cfg_attr(target_os = "uefi", no_main)]
 #![no_std]
+
+mod comms;
+mod deserializer;
+mod executor;
+mod functions;
+mod prelude;
+#[cfg(target_os = "uefi")]
+mod rt;
+mod serial;
+
+#[macro_use]
+extern crate alloc;
 
 #[cfg(not(target_os = "uefi"))]
 extern crate std;
 
-#[cfg(any(test, target_os = "uefi"))]
-mod comms;
-#[cfg(any(test, target_os = "uefi"))]
-mod deserializer;
-#[cfg(any(test, target_os = "uefi"))]
-mod executor;
-#[cfg(any(test, target_os = "uefi"))]
-mod functions;
-#[cfg(any(test, target_os = "uefi"))]
-mod prelude;
-#[cfg(target_os = "uefi")]
-mod rt;
-#[cfg(any(test, target_os = "uefi"))]
-mod serial;
-
-#[cfg(any(test, target_os = "uefi"))]
-#[macro_use]
-extern crate alloc;
+use opentmk_core::arch::serial::SerialPort;
 
 #[cfg(target_os = "uefi")]
 #[uefi::entry]
 fn uefi_entry() -> uefi::Status {
-    main();
+    _ = uefi::helpers::init();
+
+    uefi::println!("OpenTMK executor kernel");
+
+    // Note: println() will no longer work after this step
+    // since init() will exit boot services where enabled.
+    // use log from henceforth for SERIAL port 2 logging
+    match opentmk_core::uefi::init::init() {
+        Ok(_) => {
+            log::info!("OpenTMK initialization complete!");
+            main();
+        }
+        Err(e) => {
+            log::info!("OpenTMK initialization failed! - {:?}", e);
+        }
+    }
+
     uefi::Status::ABORTED
 }
 
 fn main() {
-    #[cfg(target_os = "uefi")]
-    {
-        use uefi::println;
+    let mut exec = executor::Executor::new(SerialPort::COM1);
 
-        _ = uefi::helpers::init();
-
-        println!("OpenTMK executor kernel");
-
-        // Note: println() will no longer work after this step
-        // since init() will exit boot services where enabled.
-        // use log from henceforth for SERIAL port 2 logging
-        match opentmk_core::uefi::init::init() {
-            Ok(_) => log::info!("OpenTMK initialization complete!"),
-            Err(e) => {
-                log::info!("OpenTMK initialization failed! - {:?}", e);
-                return;
-            }
-        }
+    if let Err(e) = exec.initialize() {
+        //TODO: we want to be able to catch these errors on the host side
+        log::error!("Executor initialization failed with error {:?}", e);
+        return;
     }
 
-    #[cfg(any(test, target_os = "uefi"))]
-    {
-        use opentmk_core::arch::serial::SerialPort;
+    exec.register_fuzz_functions();
 
-        let mut exec = executor::Executor::new(SerialPort::COM1);
-
-        if let Err(e) = exec.initialize() {
-            //TODO: we want to be able to catch these errors on the host side
-            log::error!("Executor initialization failed with error {:?}", e);
-            return;
-        }
-
-        exec.register_fuzz_functions();
-
-        if let Err(e) = exec.run() {
-            //TODO: we want to be able to catch these errors on the host side
-            log::error!("Executor exited with an error - {:?}", e);
-        }
+    if let Err(e) = exec.run() {
+        //TODO: we want to be able to catch these errors on the host side
+        log::error!("Executor exited with an error - {:?}", e);
     }
 }
