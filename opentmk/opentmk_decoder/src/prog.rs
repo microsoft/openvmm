8522a7e6b2b78f651afdffc1aec776b5d0ed7243
Henry Wang <wanghenry@microsoft.com>
2026-09-14 10:36:09 -0700

    formatting

--- opentmk/opentmk_decoder/src/prog.rs
+++ opentmk/opentmk_decoder/src/prog.rs
@@ -494,7 +494,11 @@ fn copyin<M: SafeMemoryMap + ?Sized>(
         }
 
         // Case: binary_format_bigendian
-        1 => return Err(DecoderError::Other(format!("unhandled: bigendian binary format"))),
+        1 => {
+            return Err(DecoderError::Other(format!(
+                "unhandled: bigendian binary format"
+            )));
+        }
 
         // Case: binary_format_strdec
         // Converts 0xffffffffffffffff into 34343736`34343831
